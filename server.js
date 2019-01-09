72a7f9a79f2a3d78df56d9c70e06b1cb04ebcb63
marb61a <marb61a@gmail.com>
2019-01-09 17:12:05 +0000

    added initial streaming and setup proper MIME types

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 
 // Serve static files from /public directory
-app.use(express.static("public"));
+app.use(express.static("public", {
+  setHeaders: (res, path, start) => {
+    // Serve wasm files with the correct MIME type
+    if(path.endsWith(".wasm")){
+      res.set("Content-Type", "application/wasm");
+    }
+  }
+}));
 
 // Start server
 app.listen(2222, () => {
